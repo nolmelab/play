63d743c0287b5a42ed8bf4c1444be78b9e300c71
jisanpack <nolmelab@gmail.com>
2024-07-19 23:46:57 +0900

    flatbuffers 보기 시작

--- play/robust/test/learn_flatbuffers.cpp
+++ play/robust/test/learn_flatbuffers.cpp
@@ -1,10 +1,33 @@
 #include <doctest/doctest.h>
+#include <flatbuffers/flatbuffers.h>
+#include "learn_fb_generated.h"
+
+using namespace App::Sample;
 
 TEST_CASE("learn flatbuffers")
 {
-  SUBCASE("compile") {}
+  SUBCASE("compile")
+  {
+    // [1] install flatbuffers-compiler
+    // [2] install flatubffers vcpkg
+    // [3] include flatbuffers.h
+    // [4] make a sample fbs
+    // - install vscode fbs extension
+    // [5] flatc --cpp file.fbs
 
-  SUBCASE("using flatc") {}
+    // version mismatch: VerifyField is not found
+    // - vcpkg version: 24.3.25
+    // - vcpkg의 buildtrees 아래의 flatc를 사용하도록 수정
+    // - 다시 fbs 컴파일 OK
+    //
+  }
+
+  SUBCASE("using generated schema")
+  {
+    flatbuffers::FlatBufferBuilder builder(1024);
+
+    SUBCASE("CreateWeapon") {}
+  }
 
   SUBCASE("object api") {}
 
