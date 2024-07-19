36da33ad3f2467b5fa3812b6d0a1fa0b77d2e43f
jisanpack <nolmelab@gmail.com>
2024-07-19 21:42:14 +0900

    learn_flatbuffers 항목 작성. 배우면서 설계

--- play/robust/test/learn_flatbuffers.cpp
+++ play/robust/test/learn_flatbuffers.cpp
@@ -2,5 +2,20 @@
 
 TEST_CASE("learn flatbuffers")
 {
-  //
+  SUBCASE("compile") {}
+
+  SUBCASE("using flatc") {}
+
+  SUBCASE("object api") {}
+
+  SUBCASE("FlatBufferBuilder") {}
+
+  SUBCASE("verification") {}
+
+  SUBCASE("how it works?") {}
+
+  SUBCASE("how to use in an app?")
+  {
+    //
+  }
 }
